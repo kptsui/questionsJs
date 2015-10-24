b3debcf235fba014e005e0fd6b9d4a6e588db50c
tinlapto <tinlapto@yahoo.com.hk>
2015-10-24 19:51:16 +0800

    rmList css update

--- js/mainPage.js
+++ js/mainPage.js
@@ -9,9 +9,9 @@ $(document).ready(function(){
 		var hyperLink = baseLink+roomName;
 
 		var courseType = roomName.substring(0,4).toLowerCase();
-		var icon = '<img src="img/'+courseType+'.png" alt="course icon">';
+		var icon = '<img src="img/'+courseType+'.png" alt="course icon" class="rmBlock">';
 
-		var stringToAppend = '<div class="col-xs-3 roomItem"><a href="'+hyperLink+'">'+icon+'<br>'+roomName.toUpperCase()+'</a></div>';
+		var stringToAppend = '<a class="col-xs-3 roomItem" href="'+hyperLink+'"><br>'+icon+'<br>'+roomName.toUpperCase()+'</a>';
 
 		roomGrid.append(stringToAppend);
 	});
