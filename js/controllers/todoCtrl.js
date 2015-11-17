67238c4adc6d5cda25fe4b78af572258a03af674
tinlapto <tinlapto@yahoo.com.hk>
2015-11-17 23:52:41 +0800

    img upload

--- js/controllers/todoCtrl.js
+++ js/controllers/todoCtrl.js
@@ -7,8 +7,11 @@
 * - exposes the model to the template and provides event handlers
 */
 todomvc.controller('TodoCtrl',
-['$scope', '$location', '$firebaseArray', '$sce', '$localStorage', '$window',
-function ($scope, $location, $firebaseArray, $sce, $localStorage, $window) {
+['$scope', '$location', '$firebaseArray', '$sce', '$localStorage', '$window', 'FileUploader',
+function ($scope, $location, $firebaseArray, $sce, $localStorage, $window, FileUploader) {
+	var uploader = $scope.uploader = new FileUploader({
+            url: 'upload.php'
+        });
 	$scope.predicate = '';
 	if(localStorage.getItem("userNameInQJS")){
 		$scope.userName = localStorage.getItem("userNameInQJS");
@@ -264,6 +267,20 @@ $scope.tagToMsg = function($tag) {
 	}
 	catch(e){
 		var Msg = $tag;
+	}
+	$scope.input = {wholeMsg: Msg};
+}
+
+$scope.imgToMsg = function($url) {
+	var url = '<img class="imgFrame" src="uploads/'+$url+'">';
+	try{
+		if ($scope.input.wholeMsg.trim())
+			var Msg = $scope.input.wholeMsg.trim() + " " +url;
+		else
+			var Msg = url;
+	}
+	catch(e){
+		var Msg = url;
 	}
 	$scope.input = {wholeMsg: Msg};
 }
@@ -528,6 +545,14 @@ $scope.toTop =function toTop() {
 	$window.scrollTo(0,0);
 };
 
+$scope.btnImgUpload = function () {
+	if ($scope.imgUpload)
+		$scope.imgUpload = false;
+	else
+		$scope.imgUpload = true;
+};
+
+
 // Not sure what is this code. Todel
 if ($location.path() === '') {
 	$location.path('/');
@@ -548,5 +573,55 @@ angular.element($window).bind("scroll", function() {
 		$scope.$apply();
 	}
 });
+
+//img uploader
+
+        // FILTERS
+
+        uploader.filters.push({
+            name: 'imageFilter',
+            fn: function(item /*{File|FileLikeObject}*/, options) {
+                var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+                return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+            }
+        });
+
+        // CALLBACKS
+
+        uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
+            console.info('onWhenAddingFileFailed', item, filter, options);
+        };
+        uploader.onAfterAddingFile = function(fileItem) {
+            console.info('onAfterAddingFile', fileItem);
+        };
+        uploader.onAfterAddingAll = function(addedFileItems) {
+            console.info('onAfterAddingAll', addedFileItems);
+        };
+        uploader.onBeforeUploadItem = function(item) {
+            console.info('onBeforeUploadItem', item);
+        };
+        uploader.onProgressItem = function(fileItem, progress) {
+            console.info('onProgressItem', fileItem, progress);
+        };
+        uploader.onProgressAll = function(progress) {
+            console.info('onProgressAll', progress);
+        };
+        uploader.onSuccessItem = function(fileItem, response, status, headers) {
+            console.info('onSuccessItem', fileItem, response, status, headers);
+        };
+        uploader.onErrorItem = function(fileItem, response, status, headers) {
+            console.info('onErrorItem', fileItem, response, status, headers);
+        };
+        uploader.onCancelItem = function(fileItem, response, status, headers) {
+            console.info('onCancelItem', fileItem, response, status, headers);
+        };
+        uploader.onCompleteItem = function(fileItem, response, status, headers) {
+            console.info('onCompleteItem', fileItem, response, status, headers);
+        };
+        uploader.onCompleteAll = function() {
+            console.info('onCompleteAll');
+        };
+
+        console.info('uploader', uploader);
 
 }]);
