f43d83aa79edaae0b60a8284451ea2fb8ef070dd
tinlapto <tinlapto@yahoo.com.hk>
2015-11-22 23:29:09 +0800

    emoji and img uploader update

--- upload.php
+++ upload.php
@@ -2,10 +2,20 @@
 
 if ( !empty( $_FILES ) ) {
 
+	$path=  $_FILES["file"]["name"];
     $tempPath = $_FILES[ 'file' ][ 'tmp_name' ];
-    $uploadPath = dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . $_FILES[ 'file' ][ 'name' ];
-
-    move_uploaded_file( $tempPath, $uploadPath );
+	$extension = ".".end(explode('.', $_FILES['file']['name']));
+	$filename= basename($path, $extension);
+	$uploadPath = 'uploads' . DIRECTORY_SEPARATOR . $filename . $extension;
+	
+	$counter = 0;
+	while (file_exists($uploadPath))
+	{
+		$counter++;
+		$uploadPath = 'uploads' . DIRECTORY_SEPARATOR . $filename . $counter . $extension;
+	}
+	
+    move_uploaded_file( $tempPath, dirname( __FILE__ ) . DIRECTORY_SEPARATOR . $uploadPath );
 
     $answer = array( 'answer' => 'File transfer completed' );
     $json = json_encode( $answer );
