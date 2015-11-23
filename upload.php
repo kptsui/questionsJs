5d777f8a5371e1e4b55f6ef25e5aee94a605be13
tinlapto <tinlapto@yahoo.com.hk>
2015-11-23 23:34:11 +0800

    debug

--- upload.php
+++ upload.php
@@ -8,13 +8,16 @@ if ( !empty( $_FILES ) ) {
 	$filename= basename($path, $extension);
 	$uploadPath = 'uploads' . DIRECTORY_SEPARATOR . $filename . $extension;
 	
-	$counter = 0;
-	while (file_exists($uploadPath))
+	$counter = "";
+	if (file_exists($uploadPath))
 	{
-		$counter++;
-		$uploadPath = 'uploads' . DIRECTORY_SEPARATOR . $filename . $counter . $extension;
+		$counter = 0;
+		while (file_exists($uploadPath))
+		{
+			$counter++;
+			$uploadPath = 'uploads' . DIRECTORY_SEPARATOR . $filename . $counter . $extension;
+		}
 	}
-	
     move_uploaded_file( $tempPath, dirname( __FILE__ ) . DIRECTORY_SEPARATOR . $uploadPath );
 
     $answer = array( 'answer' => 'File transfer completed' ,
